cf7982e1b0bce05b15234f1309d048342a8fbdcd
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-10-17 18:34:43 +0700

    add kemenag database

---
+++ generate.js
@@ -0,0 +1,269 @@
+import { program } from "commander";
+import sqlite3 from "sqlite3";
+import { writeFileSync } from "fs";
+import { basename, join } from "path";
+import { fileURLToPath } from "url";
+import { globSync } from "glob";
+
+const dbDir = fileURLToPath(new URL("./database", import.meta.url));
+
+function writePlain(data, name) {
+  writeFileSync(
+    join(dbDir, name + ".json"),
+    JSON.stringify(data, undefined, 2)
+  );
+}
+
+program.requiredOption("-i, --input <directory>").parse();
+
+const options = program.opts();
+
+function importLajnah() {
+  const db = new sqlite3.Database(join(options.input, "kemenag.lajnah"));
+
+  const verseTextData = {
+    nama: "Quran Hafs oleh Kemenag",
+    font: "LPMQ Isep Misbah",
+    verse: [],
+  };
+  const verseGundulData = {
+    nama: "Quran Gundul oleh Kemenag",
+    font: "LPMQ Isep Misbah",
+    verse: [],
+  };
+  const verseIsyaratData = {
+    nama: "Quran Isyarat oleh Kemenag",
+    font: "LPMQ MSI ISYARAT",
+    verse: [],
+  };
+  const surahData = { surah: [] };
+
+  db.serialize(() => {
+    db.each(
+      "SELECT surah, ayat, teks, gundul, isyarat FROM quran",
+      (err, row) => {
+        // console.log(row);
+        const pos = {
+          sura: parseInt(row.surah),
+          verse: parseInt(row.verse),
+        };
+        verseTextData.verse.push({
+          ...pos,
+          text: row.teks,
+        });
+        verseGundulData.verse.push({
+          ...pos,
+          text: row.gundul,
+        });
+        verseIsyaratData.verse.push({
+          ...pos,
+          text: row.isyarat,
+        });
+      },
+      () => {
+        writePlain(verseTextData, "kemenag.text.hafs");
+        writePlain(verseGundulData, "kemenag.text.gundul");
+        writePlain(verseIsyaratData, "kemenag.text.isyarat");
+      }
+    );
+
+    db.each(
+      `SELECT id, namalatin, jumlahayat, namaarab, kategory,terjemah, posisi FROM namasurat`,
+      (err, row) => {
+        surahData.surah.push({
+          index: parseInt(row.id),
+          name: row.namalatin,
+          verseCount: parseInt(row.jumlahayat),
+          arabicName: row.namaarab,
+          category: row.kategory,
+          translation: row.terjemah,
+          position: parseInt(row.posisi),
+        });
+      },
+      () => {
+        writePlain(surahData, "kemenag.extra.surah");
+      }
+    );
+  });
+
+  db.close();
+}
+
+const timesNewRomanArabToNormalFont = {
+  "\xa1": "ṣ",
+  "\xa2": "Ṣ",
+  "\xa3": "ṡ",
+  "\xa4": "Ṡ",
+  "\xa5": "ḥ",
+  "\xa6": "Ḥ",
+  "\xa7": "ẓ",
+  "\xa8": "Ẓ",
+  "\xa9": "ż",
+  "\xaa": "Ż",
+  "\xab": "ḍ",
+  "\xac": "Ḍ",
+  "\xae": "Ū",
+  "\xaf": "ṭ",
+  "\xb0": "Ṭ",
+  "\xb1": "ā",
+  "\xb2": "Ā",
+  "\xb3": "ī",
+  "\xb4": "Ī",
+  "\xb5": "ū",
+  "\xb8": "Ū",
+};
+
+function convertTimesNewRomanArabToNormalFont(text) {
+  let result = text;
+  for (const [key, value] of Object.entries(timesNewRomanArabToNormalFont)) {
+    const regex = new RegExp(key, "g");
+    result = result.replace(regex, value);
+  }
+  return result;
+}
+
+function importData() {
+  const dataDir = join(options.input, "data");
+  for (const name of globSync("*.ayt", {
+    cwd: dataDir,
+  })) {
+    const baseName = name.substring(0, name.length - ".ayt".length);
+
+    if (baseName === "en_sahih") {
+      continue;
+    }
+
+    const db = new sqlite3.Database(join(dataDir, name));
+
+    const data = {
+      nama: "",
+      verse: [],
+      footnote: [],
+    };
+
+    const isIndonesia2019 = baseName === "Indonesia2019";
+
+    const type = ["indonesia", "indonesia2019", "mandar"].includes(
+      baseName.toLowerCase()
+    )
+      ? "translation"
+      : "tafsir";
+
+    db.serialize(() => {
+      db.get("SELECT trans_name FROM trans", (err, row) => {
+        data.nama = row.trans_name;
+      });
+      let suraColumn = "sura";
+      let verseColumn = "aya";
+      if (baseName === "tahlili") {
+        suraColumn = "aya";
+        verseColumn = "juz";
+      }
+      db.each(
+        `SELECT ${suraColumn}, ${verseColumn}, text ${
+          isIndonesia2019 ? ",no_fn,text_fn" : ""
+        } FROM ${baseName}`,
+        (err, row) => {
+          const pos = {
+            sura: parseInt(row[suraColumn]),
+            verse: parseInt(row[verseColumn]),
+          };
+          let text = row.text;
+          if (baseName === "tahlili") {
+            text = convertTimesNewRomanArabToNormalFont(text);
+          }
+          if (isIndonesia2019 && row.no_fn) {
+            const noList =
+              row.no_fn === "620621"
+                ? ["620", "621"]
+                : row.no_fn === "696697"
+                ? ["696", "697"]
+                : row.no_fn.split(",");
+            const textList = row.text_fn.split("\n").reduce((acc, line) => {
+              const current = line.trim();
+              if (acc.length == 0 || /^\d+\)/.test(current)) {
+                acc.push(current);
+              } else {
+                acc[acc.length - 1] += "\n" + current;
+              }
+              return acc;
+            }, []);
+            if (noList.length != textList.length) {
+              console.log(
+                `Warning footnote index and text list not match (${pos.sura}:${pos.verse})`
+              );
+              console.log(row.no_fn);
+              console.log(row.text_fn);
+            }
+            for (let i = 0; i < noList.length; i++) {
+              let index = parseInt(noList[i]);
+              let text = textList[i];
+
+              if (index == 28 && text.startsWith("280)")) {
+                index = 280;
+              }
+
+              if (text.startsWith(`${index})`)) {
+                text = text.replace(/^\d+\)\s*/, "");
+              }
+
+              if (index == 672 && text.startsWith("(672")) {
+                text = text.replace(/^\(672\s*/, "");
+              }
+
+              noList[i] = index;
+
+              data.footnote.push({
+                ...pos,
+                index,
+                text,
+              });
+            }
+
+            for (const res of text.matchAll(/(\d+)\)/g)) {
+              const idx = parseInt(res[1]);
+              if (!noList.includes(idx)) {
+                console.log(
+                  `Verse have external foot note ${pos.sura}:${pos.verse} to ${idx}`
+                );
+              }
+            }
+          }
+
+          data.verse.push({
+            ...pos,
+            text,
+          });
+        },
+        () => {
+          writePlain(data, `kemenag.${type}.${baseName.toLowerCase()}`);
+        }
+      );
+
+      if (isIndonesia2019) {
+        const data = {
+          theme: [],
+        };
+        db.each(
+          "SELECT sura, mi, ma, tema FROM tema",
+          (err, row) => {
+            data.theme.push({
+              sura: parseInt(row.sura),
+              startVerse: parseInt(row.mi),
+              endVerse: parseInt(row.ma),
+              text: row.tema,
+            });
+          },
+          () => {
+            writePlain(data, `kemenag.extra.theme`);
+          }
+        );
+      }
+    });
+    db.close();
+  }
+}
+
+importLajnah();
+
+importData();
